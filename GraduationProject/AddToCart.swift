d2325e091a55c7b1edf4ae5c6878a823bb086974
omer <46028769+sariomer96@users.noreply.github.com>
2023-10-10 10:50:05 +0300

    view controllers created

---
+++ GraduationProject/AddToCart.swift
@@ -0,0 +1,20 @@
+//
+//  AddToCart.swift
+//  GraduationProject
+//
+//  Created by Omer on 10.10.2023.
+//
+
+import UIKit
+
+class AddToCart: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ GraduationProject/FoodDetail.swift
@@ -0,0 +1,21 @@
+//
+//  FoodDetail.swift
+//  GraduationProject
+//
+//  Created by Omer on 10.10.2023.
+//
+
+import UIKit
+
+class FoodDetail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
