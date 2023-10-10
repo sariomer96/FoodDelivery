efe7fc5973dc4b3fea70bd24b456c7532d442076
omer <46028769+sariomer96@users.noreply.github.com>
2023-10-10 12:25:47 +0300

    alamofire - RxSwift imported

---
+++ GraduationProject/data/entity/BasketFoods.swift
@@ -0,0 +1,17 @@
+//
+//  BasketFoods.swift
+//  GraduationProject
+//
+//  Created by Omer on 10.10.2023.
+//
+
+import Foundation
+
+class BasketFoods : Codable {
+    var sepet_yemek_id : Int?
+    var yemek_adi : String?
+    var yemek_resim_adi : String?
+    var yemek_fiyat : String?
+    var yemek_siparis_adet : String?
+    var kullanici_adi : String?
+}
